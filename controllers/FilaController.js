3e8524b420000ad60ae2a7dda3a920e9e36a3128
matheuefranco <matheusefranco@gmail.com>
2025-04-24 10:24:38 -0300

    feat:mostrar-fila-remover-fila

--- controllers/FilaController.js
+++ controllers/FilaController.js
@@ -5,8 +5,28 @@ function addElemento(){
        document.getElementById("txtnovoNome");
     if(!minhaFila.isFull()){
        minhaFila.enqueue(novoElemento.value);
-       minhaFila.toString();
+       mostrarFila();
+       novoElemento.value = ""; // limpa
+       novoElemento.focus(); // cursor no input
     } 
     else
         alert("Fila cheia!");     
+}// fim addElemento
+//-----------------------------------
+function mostrarFila(){
+   const listaFila = document.getElementById("listFila");
+   listaFila.textContent = minhaFila.toString();
 }
+
+//-----------------------------
+function atenderFila(){
+   if(!minhaFila.isEmpty()){
+      const atendido = minhaFila.dequeue();
+      alert("Pessoa atendida:"+atendido);
+      mostrarFila();
+      //salvar no banco texto do navegador
+   }
+   else
+      alert("Fila vazia!");
+}
+//---------------------------------------
